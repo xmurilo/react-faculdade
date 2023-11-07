09071f8deccc9839299a087c51dab773a633b1b4
Murilo <dixmurilox@gmail.com>
2023-11-07 13:57:52 -0300

    Feat adding sweet alert to see sinopse of film

--- app_filmes/src/components/CardFilme.tsx
+++ app_filmes/src/components/CardFilme.tsx
@@ -1,4 +1,4 @@
-
+import Swal from 'sweetalert2';
 interface PropsCardFilm {
   titulo: string;
   genero: string;
@@ -7,38 +7,43 @@ interface PropsCardFilm {
   preco: number;
   duracao: number;
 }
-
-const CardFilm: React.FC<PropsCardFilm> = (props) => {
+const CardFilm: React.FC<PropsCardFilm> = props => {
+  const showSinopse = (): void => {
+    Swal.fire({
+      imageUrl: `${props.foto}`,
+      imageHeight: 300,
+      imageAlt: `Poster do filme do ${props.titulo}`,
+      title: `${props.titulo}`,
+      text: `${props.sinopse}`,
+      confirmButtonText: 'Fechar',
+    });
+  };
   return (
-    <div className="col-sm-4 mb-3 mb-sm-0">
-      <div className="card mb-3">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src={props.foto}
-              className="img-fluid rounded-start"
-              alt="..."
-            />
+    <div className='col-sm-4 mb-3 mb-sm-0'>
+      <div className='card mb-3'>
+        <div className='row g-0'>
+          <div className='col-md-4'>
+            <img src={props.foto} className='img-fluid rounded-start' alt='...' />
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">{props.titulo}</h5>
-              <p className="card-text">{props.genero}</p>
-              <p className="card-text">
+          <div className='col-md-8'>
+            <div className='card-body'>
+              <h5 className='card-title'>{props.titulo}</h5>
+              <p className='card-text'>{props.genero}</p>
+              <p className='card-text'>
                 Alugue por apenas R$:
-                {props.preco.toLocaleString("pt-br", {
+                {props.preco.toLocaleString('pt-br', {
                   minimumFractionDigits: 2,
                 })}
               </p>
               <p>
                 <small>Duração: {props.duracao} min</small>
               </p>
-              <button className="btn btn-sm btn-danger">
-                Ver Sinopse <i className="bi bi-film"></i>
+              <button onClick={showSinopse} className='btn btn-sm btn-danger'>
+                Ver Sinopse <i className='bi bi-film'></i>
               </button>
 
-              <button className="btn btn-sm btn-primary float-end">
-                Alugar <i className="bi bi-cash"></i>
+              <button className='btn btn-sm btn-primary float-end'>
+                Alugar <i className='bi bi-cash'></i>
               </button>
             </div>
           </div>
